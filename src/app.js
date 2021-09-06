3ea60e3623c9c31f7d75e80bd12238a886b6deeb
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2021-09-06 17:01:01 -0500

    Setting up routes, folders and paths for folders

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors"); //adding cors
 const app = express();
+
+//createing routers
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+
+//errors handlers
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+//routes and setups
+app.use(cors());
+app.use(express.json());
+
+app.use("./movies", moviesRouter);
+app.use("./theaters", theatersRouter);
+app.use("./reviews", reviewsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
