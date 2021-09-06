a0475df0a6ae7818939e55d04a2c3f3a30101d46
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2021-09-06 18:08:54 -0500

    Created route that responds to GET /theaters
    GET /theaters with using TheaterAndMovies reducer

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
-// app.use("/theaters", theatersRouter);
+app.use("/theaters", theatersRouter);
 // app.use("/reviews", reviewsRouter);
 
 app.use(notFound);
